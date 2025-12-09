6e6b7fd00828277da2cabfe4ac34c093ea246be4
Luca2409 <luca2409@gmx.net>
2025-12-09 12:54:20 +0000

    Füge Berechnung der Dehnung hinzu

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-eingabe_1 = input("welche zahlen möchtest du addierebn")
 
+
+länge = input("wie lange")
+kraft = input("wie groß Kraft")
+dehnung = input("dehnung")
+
+ausgabe = float(dehnung) / int(länge) * 100
+
+print(ausgabe , "%")
