7d895366f3d0833cb460bad58ece249684a732ad
Luca2409 <luca2409@gmx.net>
2025-12-16 13:57:09 +0000

    Füge Temperaturüberprüfung und Ausgabe hinzu

---
+++ Übungen/Kraftberechnung.py
@@ -0,0 +1,11 @@
+hallo = "OK"
+print(bool(hallo))
+
+
+temperatur = int(input("temp:"))
+if temperatur <= 20 :
+    print("zu kalt")
+elif temperatur >= 80:
+    print("zu heiß")
+else:       
+    print("gut")    
